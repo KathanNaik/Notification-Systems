cb93b37ba5e25cc1c09639239222f3f47127f865
Kathan Naik <115002064+KathanNaik@users.noreply.github.com>
2025-12-05 18:23:47 +0530

    Update urls.py

--- templates/urls.py
+++ templates/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import TemplateCreateView
+from .views import TemplateCRUDView
 
 urlpatterns = [
-    path('create/', TemplateCreateView.as_view(), name='template-create'),
+    path('', TemplateCRUDView.as_view(), name='template-list'),
+    path('create/', TemplateCRUDView.as_view(), name='template-create'),
+    path('<pk>/', TemplateCRUDView.as_view(), name='template-detail'),
+    path('<pk>/update/', TemplateCRUDView.as_view(), name='template-update'),
+    path('<pk>/delete/', TemplateCRUDView.as_view(), name='template-delete'),
 ]
